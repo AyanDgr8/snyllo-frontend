8e5d0d034fd84a2f2d4c661dd126c218ad562e65
Ayan <ayandgr5@gmail.com>
2023-11-14 17:58:01 +0530

    addidtion

--- snyllo/src/components/routes/DetailsForm.js
+++ snyllo/src/components/routes/DetailsForm.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+// src/components/routes/DetailsForm.js
+
+import React, { useState, useEffect } from 'react';
 import './DetailsForm.css';
 
 const DetailsForm = () => {
+  const [submissionStatus, setSubmissionStatus] = useState(null);
   const [formData, setFormData] = useState({
     name: '',
     phone: '',
@@ -10,11 +13,20 @@ const DetailsForm = () => {
     location: '',
   });
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const apiUrl = 'https://snyllo-backend.onrender.com/user-details/';
 
     try {
-      const response = await fetch('Backend_url', {
+      const response = await fetch(apiUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -22,34 +34,86 @@ const DetailsForm = () => {
         body: JSON.stringify(formData),
       });
 
-      if (response.ok) {
-        console.log('User details submitted successfully');
-        // You can show a success message or redirect the user here
+      const data = await response.json().catch(() => null);
+
+      if (!response.ok || !data) {
+        console.error('Error submitting form:', data);
+        setSubmissionStatus('error');
       } else {
-        console.error('Failed to submit user details');
-        // You can show an error message here
+        console.log('Submission successful:', data);
+        setSubmissionStatus('success');
       }
     } catch (error) {
-      console.error('Error submitting user details:', error);
+      console.error('Error submitting form:', error);
+      setSubmissionStatus('error');
     }
   };
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  useEffect(() => {
+    if (submissionStatus) {
+      // Reset the form after 2 seconds
+      const resetForm = setTimeout(() => {
+        setSubmissionStatus(null);
+        setFormData({
+          name: '',
+          phone: '',
+          email: '',
+          treatment: '',
+          location: '',
+        });
+      }, 2000);
+
+      return () => clearTimeout(resetForm);
+    }
+  }, [submissionStatus]);
 
   return (
     <div className="container">
       <div className="form-part">
         <form id="form-section" onSubmit={handleSubmit}>
           <h1 className="head">Our Experts Have All The Answers For You</h1>
-          <input type="text" placeholder="Your Name" name="name" onChange={handleChange} />
-          <input type="tel" placeholder="Phone Number" name="phone" onChange={handleChange} />
-          <input type="email" placeholder="Email Address" name="email" onChange={handleChange} />
-          <input type="text" placeholder="Treatment" name="treatment" onChange={handleChange} />
-          <input type="text" placeholder="Location" name="location" onChange={handleChange} />
+          <input
+            type="text"
+            placeholder="Your Name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <input
+            type="tel"
+            placeholder="Phone Number"
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+          />
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Treatment"
+            name="treatment"
+            value={formData.treatment}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            placeholder="Location"
+            name="location"
+            value={formData.location}
+            onChange={handleChange}
+          />
           <button type="submit">Submit</button>
         </form>
+        {submissionStatus && (
+          <p className={submissionStatus === 'success' ? 'success-message' : 'error-message'}>
+            {submissionStatus === 'success' ? 'Your details are received!' : 'Error submitting form. Please try again.'}
+          </p>
+        )}
       </div>
       <div className="image-part">
         <img
